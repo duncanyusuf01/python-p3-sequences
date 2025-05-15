b8d68989ecfc0126a401dbea8ff8921e3e0e507d
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-05-15 12:44:57 +0300

    finished labs

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci=[]
+    a,b=0,1
+    for _ in range(length):
+        fibonacci.append(a)
+        a,b=b,a+b
+    print(fibonacci)
+
+print_fibonacci(9)
+
